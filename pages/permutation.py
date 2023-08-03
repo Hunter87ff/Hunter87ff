60ca8645c245a09cdaf9d84adea08003dd45cff6
hunter87 <83773586+Hunter87ff@users.noreply.github.com>
2023-08-03 11:37:00 +0530

    Create permutation.py

---
+++ pages/permutation.py
@@ -0,0 +1,51 @@
+<style>
+*{
+  padding: 0;
+  margin: 0;
+  text-align: center;
+}
+  .res{
+    text-align: center;
+    align-items: center;
+    width: 80%;
+    height: 60px;
+    background:#5e5e5e ;
+    font-size: 25px;
+  }
+  input{
+    height: 50px;
+    width: 80%;
+    padding: 10px;
+  }
+  button{
+    background: #00ff00;
+    color: #fff;
+    border-radius: 6px;
+    border: none;
+    padding: 6px;
+  }
+</style>
+
+<input class="res" value="00"></input> <br>
+<input type="number" id='n' placeholder="the value of N"> <br>
+<input type="number" id='r' placeholder="the value of R"> <br>
+<button onclick="vall()">Calculate</button> <br>
+<script>
+function nfact(n) {
+  let x = 1;
+  for (let i = 1; i <= n; i++) {
+    x *= i;
+  }
+  return x;
+};
+function vall(){
+  var d = document.querySelector(".res");
+  var n = document.querySelector('#n');
+  var r = document.querySelector('#r');
+  let p = nfact(n.value) / (nfact(n.value - r.value));
+  //alert(n, r, p)
+  d.value= parseInt(p);
+  //nfact(n) / (nfact(n=n - r));
+}
+//console.log(p);
+</script>
